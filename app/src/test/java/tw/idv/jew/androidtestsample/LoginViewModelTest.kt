7a832619a7efc0db9e2869440bd709e2ead19af4
a0916105 <whdofjew@gmail.com>
2021-06-22 21:58:01 +0800

    mockk<LoginRepository>

--- app/src/test/java/tw/idv/jew/androidtestsample/LoginViewModelTest.kt
+++ app/src/test/java/tw/idv/jew/androidtestsample/LoginViewModelTest.kt
@@ -1,6 +1,9 @@
 package tw.idv.jew.androidtestsample
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
 import org.junit.Test
@@ -13,13 +16,22 @@ class LoginViewModelTest {
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
+    private val repository = mockk<LoginRepository>(relaxed = true)
+    private val viewModel = LoginViewModel(repository)
+
     @Test
     fun test_login_success() {
-
+        coEvery { repository.login(any(), any()) }.returns(true)
+        viewModel.login("abc", "123")
+        assert(viewModel.liveData.value == true)
+        coVerify { repository.login("abc", "123") }
     }
 
     @Test
     fun test_login_fail() {
-
+        coEvery { repository.login(any(), any()) }.returns(false)
+        viewModel.login("abc", "123")
+        assert(viewModel.liveData.value == false)
+        coVerify { repository.login("abc", "123") }
     }
 }
